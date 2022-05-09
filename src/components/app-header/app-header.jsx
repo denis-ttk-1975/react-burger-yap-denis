3ea331d351240ebd3ddd70cde59c9e71db82052d
Denis Ule <denis.ttk@gmail.com>
2022-05-09 15:53:41 +0300

    complex styles applied for header

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'; // импорт библиотеки
 import ReactDOM from 'react-dom';
 
-import { Box, Icons, Typography, BurgerIcon, ListIcon, ProfileIcon, Logo } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Box, Typography, BurgerIcon, ListIcon, ProfileIcon, Logo } from '@ya.praktikum/react-developer-burger-ui-components';
 
 import styles from './app-header.module.css';
 
@@ -9,24 +9,24 @@ function AppHeader() {
   return (
     <header className={styles.header}>
       <div className={styles.leftIconBox}>
-        <div className={styles.navItemActive}>
+        <div className={`pt-4 pb-4 pl-5 pr-5 ${styles.navItemActive}`}>
           <BurgerIcon type='primary' />
-          <p className='ml-2'>Конструктор</p>
+          <p className='ml-2 text text_type_main-default'>Конструктор</p>
         </div>
 
-        <div className={styles.navItem}>
+        <div className={`pt-4 pb-4 pl-5 pr-5 ${styles.navItem}`}>
           <ListIcon type='secondary' />
-          <p className='ml-2'>Лента заказов</p>
+          <p className='ml-2 text text_type_main-default text_color_inactive'>Лента заказов</p>
         </div>
       </div>
       <div className={styles.headerLogo}>
         <Logo />
       </div>
 
-      <div className={styles.rightIconBox}>
+      <div className={`pt-4 pb-4 pl-5 pr-5 ${styles.rightIconBox}`}>
         <div className={styles.navItem}>
           <ProfileIcon type='secondary' />
-          <p className='ml-2'>Личный кабинет</p>
+          <p className='ml-2 text text_type_main-default text_color_inactive'>Личный кабинет</p>
         </div>
       </div>
     </header>
