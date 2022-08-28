6b6cc622362dba6e8e8ea55a190010dfa3169184
denis-ttk-1975 <denis.ttk@gmail.com>
2022-08-28 16:34:22 +0300

    feed card done

--- src/components/orders-feed-data/orders-feed-data.jsx
+++ src/components/orders-feed-data/orders-feed-data.jsx
@@ -1,4 +1,5 @@
 import React from 'react'; // импорт библиотеки
+import { useSelector, useDispatch } from 'react-redux';
 
 import { Box, Typography } from '@ya.praktikum/react-developer-burger-ui-components';
 
@@ -8,33 +9,36 @@ import OrdersFeedCard from './../orders-feed-card/orders-feed-card';
 
 // whole component
 function OrdersFeedData({ orders }) {
-  console.log('props from OrdersFeedData', orders);
+  const { menuIngredients: ingredientList } = useSelector((state) => state.burgerIngredients);
+
   return (
     <div className={`${styles.ordersFeed}`}>
       <h1 className={`${styles.ordersFeedTitle} text text_type_main-large`}>Лента заказов</h1>
       <div className={styles.scrollBox}>
-        {orders.map((elem) => {
-          if (!!elem._id) {
-            const orderDate = new Date(Date.parse(elem.createdAt));
-
+        {orders.map((order) => {
+          if (!!order._id) {
+            const orderDate = new Date(Date.parse(order.createdAt));
             const currentDate = new Date();
+            const date =
+              orderDate.getDate() === currentDate.getDate()
+                ? `Сегодня, ${orderDate.getHours()}:${orderDate.getMinutes()}  i-GMT+3`
+                : `Вчера, ${orderDate.getHours()}:${orderDate.getMinutes()}  i-GMT+3`;
+            const itemList = order.ingredients.map((item) => {
+              return ingredientList.find((elem) => elem._id === item.toString());
+            });
 
-            const date = orderDate.getDate() === currentDate.getDate() ? `Сегодня, ${orderDate.getHours()}:${orderDate.getMinutes()}  i-GMT+3` : 2;
+            const price = itemList.reduce((acc, item) => acc + Number(item.price), 0);
 
             return (
               <OrdersFeedCard
-                key={elem._id}
-                number={`#${elem.number}`}
+                key={order._id}
+                number={`#${order.number}`}
                 date={date}
-                title={elem.name}
-                status={elem.status}
-                data={[
-                  { image_mobile: 'https://code.s3.yandex.net/react/code/meat-02-mobile.png' },
-                  { image_mobile: 'https://code.s3.yandex.net/react/code/core-mobile.png' },
-                  { image_mobile: 'https://code.s3.yandex.net/react/code/sauce-04-mobile.png' },
-                ]}
+                title={order.name}
+                status={order.status}
+                data={itemList}
                 onClick={() => alert('You clicked on order card')}
-                price='480'
+                price={price}
               />
             );
           }
