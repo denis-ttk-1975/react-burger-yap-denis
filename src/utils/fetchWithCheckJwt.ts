3ab59e0c7ad4d36727e95aafa6174fb793ea09c2
denis-ttk-1975 <denis.ttk@gmail.com>
2022-09-26 20:49:39 +0300

    fetchWithCheckJwt !!! done

--- src/utils/fetchWithCheckJwt.ts
+++ src/utils/fetchWithCheckJwt.ts
@@ -5,13 +5,13 @@ import { postUrlUserTokenUpdate } from './url';
 
 import { getNewTokens } from './getNewTokens';
 
-export const fetchWithCheckJwt = async (url, options, checkResponsePromise, refreshToken) => {
+export const fetchWithCheckJwt = async (url: string, options: { method: string; headers: {}; body?: string }, checkResponsePromise: (res: any) => void, refreshToken: string) => {
   try {
     const res = await fetch(url, options);
     const result = await checkResponsePromise(res);
 
     return result;
-  } catch (err) {
+  } catch (err: any) {
     if (err.message === 'jwt expired' || err.message === 'invalid token' || err.message === 'jwt malformed' || err.message === 'Token is invalid') {
       await getNewTokens(postUrlUserTokenUpdate, refreshToken, checkResponsePromise);
 
