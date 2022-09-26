b0b23d71a71189ddc899e253f51e072aed5b85d0
denis-ttk-1975 <denis.ttk@gmail.com>
2022-09-26 12:40:27 +0300

    protected-root refarmed

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,6 +1,8 @@
+import { ReactNode } from 'react'; // импорт библиотеки
+
 import { Route, Redirect, useLocation } from 'react-router-dom';
 
-export function ProtectedRoute({ children, condition, redirection, ...rest }) {
+export function ProtectedRoute({ children, condition, redirection, ...rest }: { children: ReactNode; condition: boolean; redirection: string; rest: any }) {
   const location = useLocation();
   return <Route {...rest} render={() => (condition ? children : <Redirect to={{ pathname: redirection, state: { from: location } }} />)} />;
 }
