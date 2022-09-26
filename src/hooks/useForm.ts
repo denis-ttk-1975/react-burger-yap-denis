2abec0fce8ea8d81f3de81de626258b202406498
denis-ttk-1975 <denis.ttk@gmail.com>
2022-09-26 14:41:49 +0300

    hook useForm refactored

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
-export function useForm(inputValues) {
+export function useForm(inputValues: {}) {
   const [values, setValues] = useState(inputValues);
 
-  const handleChange = (event) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value, name } = event.target;
     setValues({ ...values, [name]: value });
   };
