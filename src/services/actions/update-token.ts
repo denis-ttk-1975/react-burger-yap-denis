48c3cf62f9c0311f187d9417ccf1692c8a71daf1
denis-ttk-1975 <denis.ttk@gmail.com>
2022-09-27 17:24:12 +0300

    updateToken action done

--- src/services/actions/update-token.ts
+++ src/services/actions/update-token.ts
@@ -3,24 +3,40 @@ import { checkResponse } from '../../utils/checkResponse';
 
 import { setCookie } from '../../utils/setCookie';
 
-export const SEND_UPDATE_TOKEN = 'SEND_UPDATE_TOKEN';
-export const GET_UPDATE_TOKEN_FAILED = 'GET_UPDATE_TOKEN_FAILED';
-export const GET_UPDATE_TOKEN_SUCCESS = 'GET_UPDATE_TOKEN_SUCCESS';
+import { AppDispatch } from './../../services/types/types';
+
+export const SEND_UPDATE_TOKEN: 'SEND_UPDATE_TOKEN' = 'SEND_UPDATE_TOKEN';
+export const GET_UPDATE_TOKEN_FAILED: 'GET_UPDATE_TOKEN_FAILED' = 'GET_UPDATE_TOKEN_FAILED';
+export const GET_UPDATE_TOKEN_SUCCESS: 'GET_UPDATE_TOKEN_SUCCESS' = 'GET_UPDATE_TOKEN_SUCCESS';
+
+export type TSendUpdateToken = {
+  readonly type: typeof SEND_UPDATE_TOKEN;
+};
+
+export type TGetUpdateTokenFailed = {
+  readonly type: typeof GET_UPDATE_TOKEN_FAILED;
+  readonly errorMessage: string;
+};
+
+export type TGetUpdateTokenSuccess = {
+  readonly type: typeof GET_UPDATE_TOKEN_SUCCESS;
+  readonly payload: { message: string };
+};
 
 export function setStartForUpdateTokenRequest() {
   return { type: SEND_UPDATE_TOKEN };
 }
 
-export function setFailedForUpdateTokenRequest(errorMessage) {
+export function setFailedForUpdateTokenRequest(errorMessage: string) {
   return { type: GET_UPDATE_TOKEN_FAILED, errorMessage };
 }
 
-export function setSuccessForUpdateTokenRequest(accessToken, refreshToken) {
+export function setSuccessForUpdateTokenRequest(accessToken: string, refreshToken: string) {
   return { type: GET_UPDATE_TOKEN_SUCCESS, payload: { accessToken: accessToken, refreshToken: refreshToken } };
 }
 
-export function updateTokenRequest(refreshToken) {
-  return async function (dispatch) {
+export function updateTokenRequest(refreshToken: string) {
+  return async function (dispatch: AppDispatch) {
     try {
       dispatch(setStartForUpdateTokenRequest());
 
@@ -39,7 +55,7 @@ export function updateTokenRequest(refreshToken) {
       await dispatch(setSuccessForUpdateTokenRequest(fullResponse.accessToken, fullResponse.refreshToken));
       setCookie('accessToken', fullResponse.accessToken.split('Bearer ')[1], { path: '/' });
       setCookie('refreshToken', fullResponse.refreshToken, { path: '/' });
-    } catch (error) {
+    } catch (error: any) {
       dispatch(setFailedForUpdateTokenRequest(error.message));
       Promise.reject(error.message);
     }
