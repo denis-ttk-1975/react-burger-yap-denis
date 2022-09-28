3311899848ee3226f8b04716ecc54475c5b1c78c
denis-ttk-1975 <denis.ttk@gmail.com>
2022-09-28 12:37:27 +0300

    logout reducer done

--- src/services/reducers/logout.ts
+++ src/services/reducers/logout.ts
@@ -1,11 +1,15 @@
-import { SEND_LOGOUT, GET_LOGOUT_FAILED, GET_LOGOUT_SUCCESS } from './../actions/logout';
+import { SEND_LOGOUT, GET_LOGOUT_FAILED, GET_LOGOUT_SUCCESS } from '../actions/logout';
 
-const initialState = {
+import { TLogoutActions } from './../../services/actions/logout';
+
+type TLogoutState = { isLoading: boolean; errorMessage: string };
+
+const initialState: TLogoutState = {
   isLoading: false,
   errorMessage: '',
 };
 
-export const userLogoutReducer = (state = initialState, action) => {
+export const userLogoutReducer = (state = initialState, action: TLogoutActions) => {
   switch (action.type) {
     case SEND_LOGOUT: {
       return {
