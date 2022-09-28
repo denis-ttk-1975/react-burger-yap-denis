bb5a78b1242ca09be9af2c1c905f2c2a8bca2f0c
denis-ttk-1975 <denis.ttk@gmail.com>
2022-09-28 12:40:47 +0300

    order details reducer done

--- src/services/reducers/order-details.ts
+++ src/services/reducers/order-details.ts
@@ -1,13 +1,17 @@
-import { GET_ORDER, GET_ORDER_FAILED, GET_ORDER_SUCCESS, RESET_ORDER_NUMBER, OPEN_ORDER_MODAL, CLOSE_ORDER_MODAL } from './../actions/order-details';
+import { GET_ORDER, GET_ORDER_FAILED, GET_ORDER_SUCCESS, RESET_ORDER_NUMBER, OPEN_ORDER_MODAL, CLOSE_ORDER_MODAL } from '../actions/order-details';
 
-const initialState = {
+import { TOrderDetailsActions } from './../../services/actions/order-details';
+
+type TOrderDetailsState = { isLoading: boolean; errorMessage: string; orderNumber: string; isOrderModalOpen: boolean };
+
+const initialState: TOrderDetailsState = {
   isLoading: false,
   errorMessage: '',
   orderNumber: '000000',
   isOrderModalOpen: false,
 };
 
-export const orderDetailsReducer = (state = initialState, action) => {
+export const orderDetailsReducer = (state = initialState, action: TOrderDetailsActions) => {
   switch (action.type) {
     case GET_ORDER: {
       return {
