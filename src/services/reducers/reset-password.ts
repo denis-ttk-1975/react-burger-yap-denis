44f9f9284ca8984a788c5e72bf38ebf41bc85c07
denis-ttk-1975 <denis.ttk@gmail.com>
2022-09-28 12:49:53 +0300

    resetPassword reducer done

--- src/services/reducers/reset-password.ts
+++ src/services/reducers/reset-password.ts
@@ -1,12 +1,16 @@
-import { SEND_RESET_PASSWORD, GET_RESET_PASSWORD_FAILED, GET_RESET_PASSWORD_SUCCESS } from './../actions/reset-password';
+import { SEND_RESET_PASSWORD, GET_RESET_PASSWORD_FAILED, GET_RESET_PASSWORD_SUCCESS } from '../actions/reset-password';
 
-const initialState = {
+import { TResetPasswordActions } from './../../services/actions/reset-password';
+
+type TResetPasswordState = { isLoading: boolean; errorMessage: string; message: { message: string } | {} };
+
+const initialState: TResetPasswordState = {
   isLoading: false,
   errorMessage: '',
   message: {},
 };
 
-export const resetPasswordReducer = (state = initialState, action) => {
+export const resetPasswordReducer = (state = initialState, action: TResetPasswordActions) => {
   switch (action.type) {
     case SEND_RESET_PASSWORD: {
       return {
